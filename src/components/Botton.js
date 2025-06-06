2b01b2b5faa6661b5166007704b756f6b1ac0231
alberto batista <albertomimi16@gmail.com>
2025-06-06 11:41:47 -0400

    Eliminar líneas en blanco innecesarias en Botton.js

--- src/components/Botton.js
+++ src/components/Botton.js
@@ -1,6 +1,4 @@
-
 const btn_play_template = `<button class="btn btn-primary" id="btn_play">▶ PLAY</button>`
 const btn_refresh_template = `<button class="btn btn-danger" id="btn_refresh">🔁 REFRESH</button>`
-
 
 export {btn_play_template, btn_refresh_template}
